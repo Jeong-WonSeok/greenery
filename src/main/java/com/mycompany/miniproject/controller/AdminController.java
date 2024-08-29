b8c75f274a6028dacfe2593cdb0e74fe5ac7af9a
361_seok <jws4858@gmail.com>
2024-08-30 00:26:22 +0900

    Update: Controller 경로 수정
    adminController, HomeController 경로 수정

--- src/main/java/com/mycompany/miniproject/controller/AdminController.java
+++ src/main/java/com/mycompany/miniproject/controller/AdminController.java
@@ -10,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/admin")
 public class AdminController {
 	
-	@RequestMapping("/mainadmin")
+	@RequestMapping("")
 	public String mainadmin() {
 		return "admin/mainadmin";
 	}
