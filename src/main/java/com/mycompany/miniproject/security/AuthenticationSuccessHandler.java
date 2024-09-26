32efd92e5c1a350575f14f0dea12b9b307a052f6
361_seok <jws4858@gmail.com>
2024-09-26 15:43:16 +0900

    Update : "Saved Request 기능 때문에 로그인 된 후 GET요청 가는 오류 제거"
    SavedRequestAwareAuthenticationSuccessHandler  =>
    SimpleUrlAuthenticationSuccessHandler으로 변경하여 설정함으로써 Saved Request기능 비활성화
    시킴

--- src/main/java/com/mycompany/miniproject/security/AuthenticationSuccessHandler.java
+++ src/main/java/com/mycompany/miniproject/security/AuthenticationSuccessHandler.java
@@ -7,16 +7,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class AuthenticationSuccessHandler
-	extends SavedRequestAwareAuthenticationSuccessHandler{	//로그인 성공 시 사용자가 요청한 페이지로 이동
+//	extends SavedRequestAwareAuthenticationSuccessHandler{	//로그인 성공 시 사용자가 요청한 페이지로 이동
 	// 로그인 성공 시 지정한 페이지로 이동
-	//extends SimpleUrlAuthenticationSuccessHandler{
+	extends SimpleUrlAuthenticationSuccessHandler{
+	
+    private RequestCache requestCache = new HttpSessionRequestCache();
 	
 	@Override
 	public void onAuthenticationSuccess( 	// 로그인 성공했을 때 자동으로 실행
@@ -25,7 +29,14 @@ public class AuthenticationSuccessHandler
 			Authentication authentication) throws ServletException, IOException {
 		// Authentication authentication: 로그인한 사용자의 정보를 얻을 수 있는 객체
 		// extends SimpleUrlAuthenticationSuccessHandler 를 사용할 경우 
-		// setDefaultTargetUrl("/");	//로그인 성공하면 지정한 이 경로로 이동  
+//		setDefaultTargetUrl("/");	//로그인 성공하면 지정한 이 경로로 이동  
+		SavedRequest savedRequest = requestCache.getRequest(request, response);
+		
+		if (savedRequest != null) {
+            requestCache.removeRequest(request, response);
+            getRedirectStrategy().sendRedirect(request, response, "/");
+            return;
+        }
 		log.info("실행");
 		super.onAuthenticationSuccess(request, response, authentication);
 	}
