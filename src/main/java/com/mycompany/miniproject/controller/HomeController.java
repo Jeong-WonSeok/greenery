bb6463d6e4599ed755327403677ba7fbb82292cb
361_seok <jws4858@gmail.com>
2024-09-26 17:52:54 +0900

    Update: "로그인 시 장바구니에 있던 목록들 장바구니DB로 이동"
    로그인 시 장바구니에 있던 목록들 장바구니DB로 이동

--- src/main/java/com/mycompany/miniproject/controller/HomeController.java
+++ src/main/java/com/mycompany/miniproject/controller/HomeController.java
@@ -1,19 +1,23 @@
 package com.mycompany.miniproject.controller;
 import java.io.OutputStream;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.mycompany.miniproject.dto.CartDto;
 import com.mycompany.miniproject.dto.Pager;
 import com.mycompany.miniproject.dto.ProductDto;
 import com.mycompany.miniproject.dto.ProductImageDto;
+import com.mycompany.miniproject.service.CartService;
 import com.mycompany.miniproject.service.ProductService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -24,17 +28,36 @@ public class HomeController {
 	
 	@Autowired
 	private ProductService productService;
+	@Autowired
+	private CartService cartService;
 	
 	@GetMapping("/")
 	public String mainPage( 
 						@RequestParam(defaultValue="1") int pageNo,
 						@RequestParam(name="sort", defaultValue="default") String sort,
+						Authentication authentication,
 						HttpSession session, 
 						Model model) {
 		
 		int totalRows = productService.getTotalRows();
 		Pager pager = new Pager(10, 5, totalRows, pageNo);
 		session.setAttribute("pager", pager);
+		
+		if(authentication != null) {
+			String userId = authentication.getName();
+			Map<Integer, Integer> cartList = (Map<Integer, Integer>) session.getAttribute("cartList");
+			if(cartList != null) {
+				for(Map.Entry<Integer, Integer> entry : cartList.entrySet()) {
+					int productId = entry.getKey();
+					int productQty = entry.getValue();
+					CartDto cartDto = cartService.getCartInfo(productId, userId);
+					if(cartDto == null) {
+						cartService.cartAdd(productQty, productId, userId);
+					}
+				}
+				session.removeAttribute("cartList");
+			}
+		}
 		
 		List<ProductDto> productList = productService.getProductAll(pager, sort);
 		model.addAttribute("productList", productList);
